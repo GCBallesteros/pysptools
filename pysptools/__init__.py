e26c13da55fb4be0218eb8753d5b99261a19bb97
Guillem Ballesteros <guillem@maxwellrules.com>
2023-10-17 10:32:57 +0100

    Add Kuva greeting message

--- pysptools/__init__.py
+++ pysptools/__init__.py
@@ -3,3 +3,5 @@
 __author__ = "Christian Therien <ctherien@users.sourceforge.net>"
 __license__ = "Apache License Version 2.0"
 __version__ = '0.15.0' # beta
+
+print("Hello from Kuva Space's pysptools fork!")
